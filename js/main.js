4a770706b371c596e3c56f20e7dc51ae0aad3e62
Dan Michaelson <dan@linkedbyair.net>
2014-02-22 18:03:28 -0500

    First stab at video with rollover

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
+/* VIDEO ROLLOVER */
+
+$(document).ready(function() {
+
+    $("video.module:not(#jenny)").hover(function() {
+        this.play();
+    }, function() {
+        this.pause();
+    })
+
+});
+
 /* JONATHAN */
 
-$(document).ready(function(){
+$(document).ready(function() {
 
     var divsize = ((Math.random()*100) + 50).toFixed();
 
@@ -9,7 +21,7 @@ $(document).ready(function(){
         $(this).width("800px");
         $(this).height("800px");
         $(this).css({"position":"absolute","top":"0","left":"0"});
-        $('img').addClass('rotated');
+        $("#jonathan img").addClass('rotated');
         $(".pix").animate({
             "margin-left": '+=50',
             "margin-top": '+=50',
@@ -20,7 +32,7 @@ $(document).ready(function(){
         $(this).width("160px");
         $(this).height("70px");
         $(this).css("position","static");
-        $('img').removeClass('rotated');
+        $("#jonathan img").removeClass('rotated');
         $(".pix").animate({
             "margin-left": '-=50',
             "margin-top": '-=50',
